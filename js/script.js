a554b1564751f5276681b5d88299aba37318962a
demopylsar <45366789+demopylsar@users.noreply.github.com>
2018-12-04 21:56:20 +0300

    asdasdasdwadasdasdawdawd

--- js/script.js
+++ js/script.js
@@ -7,3 +7,98 @@ $(function() {
     }
   });
 });
+
+
+
+
+
+
+ if( window.innerWidth <= 768 ){
+    $(document).ready(function(){
+  	$(".owl-carousel").owlCarousel();
+});
+ } else {
+      //не выполнять
+ } 
+
+
+
+
+
+$('.js-close-campaign').click(function() { 
+  
+  $('.overflow').addClass('disab');
+
+});
+
+
+
+$('.js-open-campaign').click(function() { 
+  
+  $('.overflow').removeClass('disab');
+
+});
+
+
+
+
+
+$('.qweryti1').click(function() { 
+  
+  $('.overfj1').addClass('disab');
+
+});
+
+$('.fo1').click(function() { 
+  
+  $('.overfj1').removeClass('disab');
+
+});
+
+
+
+
+
+$('.qweryti2').click(function() { 
+  
+  $('.overfj2').addClass('disab');
+
+});
+
+$('.fo2').click(function() { 
+  
+  $('.overfj2').removeClass('disab');
+
+});
+
+
+
+
+
+$('.qweryti3').click(function() { 
+  
+  $('.overfj3').addClass('disab');
+
+});
+
+$('.fo3').click(function() { 
+  
+  $('.overfj3').removeClass('disab');
+
+});
+
+
+
+
+
+$('.jlos').click(function() { 
+  
+  $('.mmmennu').addClass('disab');
+
+});
+
+$('.back').click(function() { 
+  
+  $('.mmmennu').removeClass('disab');
+
+});
